aa9a8d9160fd4a4158ec8dffcff4e340f0c99919
chany63 <44467300+chany63@users.noreply.github.com>
2021-11-22 22:43:17 +0900

    add About-App to source

---
+++ About-App.php
@@ -0,0 +1,140 @@
+<?
+include "config.php";
+include "util.php";
+?>
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="INTUITIVE">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>About App</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="About-App.css" media="screen">
+<link rel="stylesheet" href="styles.css">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.30.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+
+
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/1.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="About App">
+    <meta property="og:type" content="website">
+  </head>
+  <form action="About-team.php" method="post">
+    <input
+      type="text"
+      name="search_keyword"
+      placeholder="직원 검색(이름)"
+    />
+  </form>
+
+  <form action="Patients.php" method="post">
+    <input
+      type="text"
+      name="search_keyword"
+      placeholder="환자 검색(이름)"
+    />
+  </form>
+
+  <form action="Hospitalization.php" method="post">
+    <input
+      type="text"
+      name="search_keyword"
+      placeholder="입원환자 검색(이름)"
+    />
+  </form>
+
+  <form action="Medication-tasks.php" method="post">
+    <input
+      type="text"
+      name="search_keyword"
+      placeholder="치료업무 검색(직원번호)"
+    />
+  </form>
+  <body class="u-body"><header class="u-clearfix u-header u-header" id="sec-d9f7"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="Home.php" data-page-id="25047863" class="u-image u-logo u-image-1" title="Home" data-image-width="1425" data-image-height="398">
+          <img src="images/1.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="About-App.php" style="padding: 10px 20px;">About App</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="About-team.php" style="padding: 10px 20px;">About team</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Medication-tasks.php" style="padding: 10px 20px;">Medication tasks</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Patients.php" style="padding: 10px 20px;">Patients</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Hospitalization.php" style="padding: 10px 20px;">Hospitalization</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="About-App.php" style="padding: 10px 20px;">About App</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="About-team.php" style="padding: 10px 20px;">About team</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Medication-tasks.php" style="padding: 10px 20px;">Medication tasks</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Patients.php" style="padding: 10px 20px;">Patients</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Hospitalization.php" style="padding: 10px 20px;">Hospitalization</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-align-center u-clearfix u-section-1" id="sec-812b">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1">
+                  <h2 class="u-text u-text-grey-70 u-text-1">Contact me</h2>
+                  <p class="u-text u-text-grey-50 u-text-2">Sungbukgu Anamdong 5<br>Korea University 18th<br>chany63@korea.ac.kr<br>
+                  </p>
+                  <div class="u-social-icons u-spacing-20 u-social-icons-1">
+                    <a class="u-social-url" target="_blank" href="facebook.com" title="facebook.com"><span class="u-icon u-icon-circle u-social-facebook u-social-icon u-icon-1">
+                    <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-50ee"></use></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 112 112" x="0px" y="0px" id="svg-50ee"><path d="M75.5,28.8H65.4c-1.5,0-4,0.9-4,4.3v9.4h13.9l-1.5,15.8H61.4v45.1H42.8V58.3h-8.8V42.4h8.8V32.2 c0-7.4,3.4-18.8,18.8-18.8h13.8v15.4H75.5z"></path></svg>
+                  </span>
+                    </a>
+                    <a class="u-social-url" target="_blank" href="instagram.com" title="instagram.com"><span class="u-icon u-icon-circle u-social-icon u-social-instagram u-icon-2">
+                    <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-dd2a"></use></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 112 112" x="0px" y="0px" id="svg-dd2a"><path d="M55.9,32.9c-12.8,0-23.2,10.4-23.2,23.2s10.4,23.2,23.2,23.2s23.2-10.4,23.2-23.2S68.7,32.9,55.9,32.9z M55.9,69.4c-7.4,0-13.3-6-13.3-13.3c-0.1-7.4,6-13.3,13.3-13.3s13.3,6,13.3,13.3C69.3,63.5,63.3,69.4,55.9,69.4z"></path><path d="M79.7,26.8c-3,0-5.4,2.5-5.4,5.4s2.5,5.4,5.4,5.4c3,0,5.4-2.5,5.4-5.4S82.7,26.8,79.7,26.8z"></path><path d="M78.2,11H33.5C21,11,10.8,21.3,10.8,33.7v44.7c0,12.6,10.2,22.8,22.7,22.8h44.7c12.6,0,22.7-10.2,22.7-22.7 V33.7C100.8,21.1,90.6,11,78.2,11z M91,78.4c0,7.1-5.8,12.8-12.8,12.8H33.5c-7.1,0-12.8-5.8-12.8-12.8V33.7 c0-7.1,5.8-12.8,12.8-12.8h44.7c7.1,0,12.8,5.8,12.8,12.8V78.4z"></path></svg>
+                  </span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
+                <div class="u-container-layout u-container-layout-2"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-7dd8"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Contact Phone: 01063109906 Email: chany63@korea.ac.kr<br>Address: Seoul Sungbukgu Anamdong 5&nbsp;
+        </p>
+      </div></footer>
+  </body>
+</html>
